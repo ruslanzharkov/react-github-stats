fb8bdb3bec40f397614facdaf9ee5d7d3079fdcc
loki <rus_basket_kg@inbox.ru>
2018-10-25 21:21:40 +0600

    Added importing index.js

--- src/components/Starred/Starred.js
+++ src/components/Starred/Starred.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import Input from '../_common/Input/Input';
+import Input from '../_common/Input/index';
 import './Starred.css'
 import Button from '../_common/Button';
 
